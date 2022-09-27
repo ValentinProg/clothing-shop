bcdbc6290c59e01e33e6ea7bb6884938a41e936d
Valentin <V.Kovalenko.prog@gmail.com>
2022-09-27 13:30:48 +0300

    work with navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
 import Home from "./routes/home/home.component";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 
-  const Shop = () => {
-    return <h1>I am Shop!!!</h1>
-  }
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am the navigation bar</h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
 
-
+const Shop = () => {
+  return <h1>I am Shop!!!85</h1>;
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home/>}>
-      <Route path='/shop' element={<Shop/>}/>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
       </Route>
     </Routes>
   );
 };
 
 export default App;
-
-
