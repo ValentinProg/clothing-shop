151cf2535089f0907ab0731a098eeaaa46d0e9da
Valentin <V.Kovalenko.prog@gmail.com>
2022-10-04 09:59:28 +0300

    add new Shop page

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -79,4 +79,5 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
 export const signOutUser = async () => await signOut(auth);
 
 export const onAuthStateChangedListener = (callback) => 
-  onAuthStateChanged(auth, callback, errorCallback, completedCallback);
+  onAuthStateChanged(auth, callback);
+  // errorCallback, completedCallback,
